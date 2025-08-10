39450e46f3f114e9c34a171eacda5f0926bc0454
a.mohammadi <mohammadiahad93@gmail.com>
2025-08-10 16:42:50 +0330

    style: password show toggle

--- components/FormField.tsx
+++ components/FormField.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import {
   FormControl,
   FormDescription,
@@ -8,6 +8,8 @@ import {
 } from "./ui/form";
 import { Input } from "./ui/input";
 import { Control, Controller, FieldValues, Path } from "react-hook-form";
+import { Eye, EyeOff } from "lucide-react"; // آیکون‌ها
+
 interface FormFieldProps<T extends FieldValues> {
   control: Control<T>;
   name: Path<T>;
@@ -16,7 +18,8 @@ interface FormFieldProps<T extends FieldValues> {
   type?: "text" | "email" | "password" | "file";
   description?: boolean;
 }
-function FormField({
+
+function FormField<T extends FieldValues>({
   control,
   name,
   label,
@@ -24,6 +27,8 @@ function FormField({
   description = false,
   type = "text",
 }: FormFieldProps<T>) {
+  const [showPassword, setShowPassword] = useState(false);
+
   return (
     <Controller
       control={control}
@@ -32,16 +37,31 @@ function FormField({
         <FormItem>
           <FormLabel className="label">{label}</FormLabel>
           <FormControl>
-            <Input
-              className="input"
-              placeholder={placeholder}
-              {...field}
-              type={type}
-            />
+            <div className="relative">
+              <Input
+                className="input pr-10" // فضای اضافه برای آیکون
+                placeholder={placeholder}
+                {...field}
+                type={type === "password" && !showPassword ? "password" : "text"}
+              />
+              {type === "password" && (
+                <button
+                  type="button"
+                  className="absolute inset-y-0 right-2 flex items-center text-gray-400 hover:text-background"
+                  onClick={() => setShowPassword((prev) => !prev)}
+                >
+                  {showPassword ? (
+                    <EyeOff className="w-5 h-5" />
+                  ) : (
+                    <Eye className="w-5 h-5" />
+                  )}
+                </button>
+              )}
+            </div>
           </FormControl>
           {description && (
             <FormDescription className="text-amber-200">
-              {type === "password" && " at least 6 characters"}
+              {type === "password" && "at least 6 characters"}
             </FormDescription>
           )}
           <FormMessage />
